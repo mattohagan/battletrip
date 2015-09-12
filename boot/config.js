0255af7b716117e14e78a4f680c033dfd442ed1d
Apple <mattieoha@gmail.com>
2015-09-12 18:04:20 -0400

    start boilerplate

--- boot/config.js
+++ boot/config.js
@@ -3,10 +3,6 @@ var logfmt = require('logfmt');
 var bodyParser = require('body-parser');
 
 module.exports = function(app){
-	app.set('views', __dirname + '/../app/views');
-	app.set('view engine', 'jade');
-	app.locals.pretty = true;
-
 	// logger
 	app.use(logfmt.requestLogger());
 	// for routing and rendering
@@ -16,10 +12,6 @@ module.exports = function(app){
 	}));
 
 
-	app.use("/public", app.express.static(__dirname + '/../public'));
-	app.use("/js", app.express.static(__dirname + '/../public/js'));
-	app.use("/css", app.express.static(__dirname + '/../public/css'));
-	app.use("/img", app.express.static(__dirname + '/../public/img'));
-	app.use("/fonts", app.express.static(__dirname + '/../public/fonts'));
+
 
 }
--- boot/routes.js
+++ boot/routes.js
@@ -1,12 +1,51 @@
 
 module.exports = function(app){
 
+	// not much purpose
 	app.get('/', function(req, res) {
-		res.render('index');
+		res.send('hello');
 	});
 
-	app.get('/hackfsu', function(req, res){
-		res.render('hackfsu');
+	// benji getting coordinates for miss map
+	app.get('/misses', function(req, res){
+		// get miss data from parse
+
+		var coordinates = {
+			arr: [
+				{lat: 4, lon: 10},
+				{lat: 5, lon: 10},
+				{lat: 4, lon: 10}
+			]
+		};
+
+		res.send(coordinates);
+	});
+
+	// receiving final destination from user
+	app.get('/destination', function(req, res){
+
+		// hardcode these values
+		var lat = req.body.lat;
+		var lon = req.body.lon;
+		console.log('hello');
+
+
+
+		// get parse data
+		// check with geo fencing here
+
+		// if hit
+		// send notification somehow to parse cloud code to send push notification to that user 
+		// store hit in parse and decrease size of radius?
+
+		// if miss
+		// send notification of miss
+		// store miss in parse
+
+
+		res.end();
+
+
 	});
 
 
